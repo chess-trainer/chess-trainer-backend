9a3dda5ed22b2715bf9d972e678bf5b7001cef53
9KelvinN <9kelvin@gmail.com>
2020-06-15 22:29:16 -0400

    Created a problem class & loop for multiple pages
    The class contains a problem's attributes (Chess FEN,id, Rating, number
    of Attempts, PassPercentage, the Number of Moves, the Average playing
    time, and the color to play as. The loop will allow puzzles to be
    scraped from all 6720 HTML pages.

--- src/PuzzleScraper.py
+++ src/PuzzleScraper.py
@@ -2,21 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-page = requests.get('https://www.chess.com/puzzles/problems?fbclid=IwAR1GA26iG8uMy5gxWRp1X8X3nYBzSExelBBsKjEZev2Gl9JKxxlkGjIZaSU')
-soup = BeautifulSoup(page.content, 'html.parser')
-
-problems = soup.find_all(class_='v-board-popover')
-
-for problem in problems:
-    problem.get('v-board-popover')
-    try:
-        FEN = re.search("fen: '(.+?) ", problem.get('v-board-popover')).group(1)
-    except AttributeError:
-        FEN =''
-    ID, Rating, Attempts, PassPercent, NumMoves, AvgTime, PlayAs = problem.find_all('a')
-    print(FEN)
-    print(Rating.text.strip())
-    print(NumMoves.text.strip())
-    print(PlayAs.text.strip())
-
+class problem:
+    def __init__(self, fen, attributes):
+        self.FEN = fen
+        self.ID = attributes[0].text.strip()
+        self.Rating = attributes[1].text.strip()
+        self.Attempts = attributes[2].text.strip()
+        self.PassPercent = attributes[3].text.strip()
+        self.NumMoves = attributes[4].text.strip()
+        self.AvgTime = attributes[5].text.strip()
+        self.PlayAsColor = attributes[6].text.strip()
+#6720 pages in total
+problems=[]
+for pagenum in range(1,4):
+    page = requests.get('https://www.chess.com/puzzles/problems?page=' + str(pagenum))
+    soup = BeautifulSoup(page.content, 'html.parser')
     
+    problemshtml = soup.find_all(class_='v-board-popover')    
+    for problemhtml in problemshtml:
+        problemhtml.get('v-board-popover')
+        try:
+            fen = re.search("fen: '(.+?) ", problemhtml.get('v-board-popover')).group(1)
+        except AttributeError:
+            fen =''
+        #The attributes are id, Rating, Attempts, PassPercent, NumMoves, AvgTime, PlayAsColor
+        attributes = problemhtml.find_all('a')
+        problems.append(problem(fen,attributes))
+#Test
+print(problems[11].FEN, problems[11].ID, problems[11].Rating, problems[11].Attempts, problems[11].PassPercent,\
+       problems[11].NumMoves, problems[11].AvgTime, problems[11].PlayAsColor)
+

0b0d6015b2ba89e5a2bb8bd4e86f5dca48bc2f29
9KelvinN <9kelvin@gmail.com>
2020-06-15 23:48:00 -0400

    Changed some problem attributes to int and removed testing numbers.
    Specifically the ID, rating, attempts, and number of moves attribute.

--- src/PuzzleScraper.py
+++ src/PuzzleScraper.py
@@ -5,16 +5,16 @@ import re
 class problem:
     def __init__(self, fen, attributes):
         self.FEN = fen
-        self.ID = attributes[0].text.strip()
-        self.Rating = attributes[1].text.strip()
-        self.Attempts = attributes[2].text.strip()
+        self.ID = int(attributes[0].text.strip())
+        self.Rating = int(attributes[1].text.strip())
+        self.Attempts = int(attributes[2].text.strip())
         self.PassPercent = attributes[3].text.strip()
-        self.NumMoves = attributes[4].text.strip()
+        self.NumMoves = int(attributes[4].text.strip())
         self.AvgTime = attributes[5].text.strip()
         self.PlayAsColor = attributes[6].text.strip()
-#6720 pages in total
+#6722 pages in total
 problems=[]
-for pagenum in range(1,4):
+for pagenum in range(1,6723):
     page = requests.get('https://www.chess.com/puzzles/problems?page=' + str(pagenum))
     soup = BeautifulSoup(page.content, 'html.parser')
     
@@ -29,6 +29,6 @@ for pagenum in range(1,4):
         attributes = problemhtml.find_all('a')
         problems.append(problem(fen,attributes))
 #Test
-print(problems[11].FEN, problems[11].ID, problems[11].Rating, problems[11].Attempts, problems[11].PassPercent,\
-       problems[11].NumMoves, problems[11].AvgTime, problems[11].PlayAsColor)
+#print(problems[11].FEN, problems[11].ID, problems[11].Rating, problems[11].Attempts, problems[11].PassPercent,\
+#       problems[11].NumMoves, problems[11].AvgTime, problems[11].PlayAsColor)
 

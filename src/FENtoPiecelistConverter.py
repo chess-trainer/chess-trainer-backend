0a1b70faf304c68774f6f6517d9ed2584de22579
thisistrivial <g.lee13770334@gmail.com>
2020-06-17 00:16:57 -0400

    Added more functionality to fen converter: RAW and ALGEBRAIC notation modes; lambda function on string (for lowercase and uppercase). Removed test case in source code.

--- src/FENtoPiecelistConverter.py
+++ src/FENtoPiecelistConverter.py
@@ -1,33 +1,37 @@
-files="abcdefgh"
-def getPiecelist(fen):
-    whitePiecelist=[]
-    blackPiecelist=[]
-    ranks = fen.split('/')
-    rowcounter= 9
-    for rank in ranks:
-        rowcounter-=1
-        filecounter = 1
-        for curChar in rank:
-            if curChar.isdigit():
-                filecounter+= int(curChar)
-                continue
-            elif curChar.islower():
-                if curChar == 'p':
-                    blackPiecelist.append(files[filecounter-1] + str(rowcounter))
-                else:
-                    blackPiecelist.append(curChar.upper() + files[filecounter-1] + str(rowcounter))
-            else:
-                if curChar == 'P':
-                    whitePiecelist.append(files[filecounter-1] + str(rowcounter))
-                else:
-                    whitePiecelist.append(curChar + files[filecounter-1] + str(rowcounter))
-            filecounter+=1
-    return (whitePiecelist, blackPiecelist)
+from enum import Enum
 
-# Test
-# whitePieces,blackPieces = getPiecelist('1r4kR/1p2nrb1/p2qp1p1/4NpQ1/3P4/3B2P1/PP3P2/1K5R')   
-# for whitePiece in whitePieces:
-#     print(whitePiece)
-# print('\n')
-# for blackPiece in blackPieces:
-#     print(blackPiece)     
+class Notation(Enum):
+  RAW=0
+  ALGEBRAIC=1
+
+def __fenToPiecelist(fen, lambda_map=lambda s: s.upper(), notation_type=Notation.RAW):
+  files="abcdefgh"
+  whitePiecelist=[]
+  blackPiecelist=[]
+  ranks = fen.split('/')
+  rowcounter= 9
+  for rank in ranks:
+    rowcounter-=1
+    filecounter = 1
+    for curChar in rank:
+      if curChar.isdigit():
+        filecounter+= int(curChar)
+        continue
+      elif curChar.islower():
+        if notation_type == Notation.ALGEBRAIC and curChar == 'p':
+          blackPiecelist.append(files[filecounter-1] + str(rowcounter))
+        else:
+          blackPiecelist.append((lambda_map (curChar)) + files[filecounter-1] + str(rowcounter))
+      else:
+        if notation_type == Notation.ALGEBRAIC and curChar == 'P':
+          whitePiecelist.append(files[filecounter-1] + str(rowcounter))
+        else:
+          whitePiecelist.append((lambda_map (curChar)) + files[filecounter-1] + str(rowcounter))
+      filecounter+=1
+  return (whitePiecelist, blackPiecelist)
+ 
+def fenToAlgebraicPiecelist(fen, lambda_map=lambda s: s.upper()):
+  return __fenToPieceList(fen, lambda_map=lambda_map, notation_type=Notation.ALGEBRAIC)
+
+def fenToRawPiecelist(fen, lambda_map=lambda s: s.upper()):
+  return __fenToPieceList(fen, lambda_map=lambda_map, notation_type=Notation.RAW)

cb3a97eeb77c485e17094840ed7e0063979b040e
thisistrivial <g.lee13770334@gmail.com>
2020-07-05 17:46:27 -0400

    Added db tactics handler

--- src/database_management/tactics_handler.py
+++ src/database_management/tactics_handler.py
@@ -1,12 +1,17 @@
-import db_handler_persistent as db
+import rand
 
-conn = None
+# TODO ALL
 
-def connect():
-  global conn
-  conn = db.connect("tactics.db")
+import db_handler as db
 
-def new_tactic(tactic_id, fen, rating, to_move, solution):
-  global conn
-  tactics_columns = ("id", "fen", "rating", "to_move", "solution")
-  db.insert(conn, "tactics", tactics_columns, (tactic_id, fen, rating, to_move, solution)) 
+def get_tactic(rating):
+  db.select("tactics.db", "tactics", "*", "ORDER by abs()")
+  id, fen, rating, to_move, solution
+
+  user_columns = ("id", "username", "password", "email")
+  user_stats_columns = ("id", "visual_tactics_rating")
+  user_id = rand.randInt(min_id, max_id) 
+  while len(db.select"users.db", "users", "*", "id=" + str(user_id)) != 0:
+    user_id = rand.randInt(min_id, max_id)
+  db.insert("users.db", "users", user_columns, (user_id, username, password, email)) 
+  db.insert("users.db", "user_stats", user_stats_columns, (user_id, 1200)) 

43c26228fa5ba5dd2a3e2c517c4b20c90f9a1000
thisistrivial <g.lee13770334@gmail.com>
2020-07-05 17:55:05 -0400

    Wrote get function for getting a tactic

--- src/database_management/tactics_handler.py
+++ src/database_management/tactics_handler.py
@@ -1,17 +1,10 @@
-import rand
-
-# TODO ALL
+import random
 
 import db_handler as db
 
-def get_tactic(rating):
-  db.select("tactics.db", "tactics", "*", "ORDER by abs()")
-  id, fen, rating, to_move, solution
+RATING_RANGE = 200
 
-  user_columns = ("id", "username", "password", "email")
-  user_stats_columns = ("id", "visual_tactics_rating")
-  user_id = rand.randInt(min_id, max_id) 
-  while len(db.select"users.db", "users", "*", "id=" + str(user_id)) != 0:
-    user_id = rand.randInt(min_id, max_id)
-  db.insert("users.db", "users", user_columns, (user_id, username, password, email)) 
-  db.insert("users.db", "user_stats", user_stats_columns, (user_id, 1200)) 
+def get_tactic(rating):
+  # columns are: id, fen, rating, to_move, solution
+  tactics = db.select("tactics.db", "tactics", "*", "WHERE abs(rating - " + str(rating) + ") < " + str(RATING_RANGE))
+  return random.choice(tactics) 
